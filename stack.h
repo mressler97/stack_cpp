99e7466e4fd8ec66e8dcd803862d51b3fbe6b52f
Michael <michael_ressler@ymail.com>
2019-05-03 05:11:02 -0600

    header file comments

--- stack.h
+++ stack.h
@@ -1,15 +1,9 @@
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
- /*
-  * File:   stack.h
-  * Author: k57h721
+ /* Name:		Michael Ressler
+  * File:		Stack Header
+  * Program:	Stack.cpp
   *
-  * Created on April 4, 2019, 11:10 AM
+  * 
   */
 
 #ifndef STACK_H
